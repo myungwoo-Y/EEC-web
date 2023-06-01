933af1349899dc8013ab6f301d6dc812006a0b88
myungwoo-Y <auddn6676@naver.com>
2023-06-02 02:50:46 +0900

    FE: add input component

---
+++ client/components/Input.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+interface InputProps {
+  className?: string,
+  onChange?: () => void
+  placeholder?: string,
+  type: 'email' | 'password'
+}
+
+export default function Input({ className = '', onChange, placeholder, type }: InputProps) {
+  return (
+    <>
+      <input
+        className={
+          className + 'border-[1px] border-gray-300 rounded-md w-72 py-1 px-2'
+        }
+        type={type}
+      />
+    </>
+  );
+}

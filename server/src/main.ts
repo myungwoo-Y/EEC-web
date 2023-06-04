1f5709bfb39199217716b96e55b0b6d9d27193cb
myungwoo-Y <auddn6676@naver.com>
2023-06-04 15:23:19 +0900

    BE: fix port

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
 
 async function bootstrap() {
+  config();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();

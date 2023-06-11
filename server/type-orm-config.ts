e041fd66d25341fe050ab252a4e72c9e1c65fad3
myungwoo-Y <auddn6676@naver.com>
2023-06-11 14:41:47 +0900

    BE: reset migration files

--- server/type-orm-config.ts
+++ server/type-orm-config.ts
@@ -1,6 +1,8 @@
 import { configService } from 'src/config/config.service';
 import { DataSource } from 'typeorm';
 import { User } from 'src/model/user.entity';
+import { Board } from 'src/model/board.entity';
+import { BoardCategory } from 'src/model/boardCategory.entity';
 
 
 export default new DataSource({
@@ -10,8 +12,8 @@ export default new DataSource({
   username: configService.getValue('POSTGRES_USER'),
   password: configService.getValue('POSTGRES_PASSWORD'),
   database: configService.getValue('POSTGRES_DATABASE'),
-  entities: [User],
-  migrationsTableName: 'eec_web_local',
+  entities: [User, Board, BoardCategory],
+  migrationsTableName: 'eec_web_migration',
   migrations: ['migrations/*{.ts,.js}'],
   synchronize: false,
 });

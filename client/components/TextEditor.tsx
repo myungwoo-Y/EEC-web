f027b6e5fb918e21ae0f859fcf0ce9eb811f3583
myungwoo-Y <auddn6676@naver.com>
2023-06-18 14:25:37 +0900

    Fix text editor menu

--- client/components/TextEditor.tsx
+++ client/components/TextEditor.tsx
@@ -1,10 +1,6 @@
 import {
   ArrowLeftCircleIcon,
   ArrowRightCircleIcon,
-  ArrowUturnLeftIcon,
-  ArrowUturnRightIcon,
-  ChevronLeftIcon,
-  ChevronRightIcon,
   LinkIcon,
 } from '@heroicons/react/24/outline';
 import { Color } from '@tiptap/extension-color';
@@ -12,7 +8,8 @@ import ListItem from '@tiptap/extension-list-item';
 import TextStyle from '@tiptap/extension-text-style';
 import { EditorContent, useEditor, Editor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
-import React from 'react';
+import Link from '@tiptap/extension-link';
+import React, { useCallback } from 'react';
 import BoldIcon from 'remixicon-react/BoldIcon';
 import ItalicIcon from 'remixicon-react/ItalicIcon';
 import StrikethroughIcon from 'remixicon-react/StrikethroughIcon';
@@ -30,18 +27,38 @@ const MenuBar = ({ editor }: { editor: Editor | null }) => {
     return null;
   }
 
+  const setLink = useCallback(() => {
+    const previousUrl = editor.getAttributes('link').href
+    const url = window.prompt('URL', previousUrl)
+
+    // cancelled
+    if (url === null) {
+      return
+    }
+
+    // empty
+    if (url === '') {
+      editor.chain().focus().extendMarkRange('link').unsetLink()
+        .run()
+
+      return
+    }
+
+    // update link
+    editor.chain().focus().extendMarkRange('link').setLink({ href: url })
+      .run()
+  }, [editor])
+
   return (
     <div className="p-2 flex items-center">
-      <button
-        onClick={() => editor.chain().focus().redo().run()}
-        disabled={!editor.can().chain().focus().redo().run()}
+      <button 
+        onClick={setLink}
         className="w-8 h-8 text-center text-gray-500 disabled:text-gray-300 hover:bg-gray-100 p-1 rounded-md  flex items-center"
       >
         <LinkIcon width={20} />
       </button>
       <button
         onClick={() => editor.chain().focus().toggleBold().run()}
-        disabled={!editor.can().chain().focus().toggleBold().run()}
         className="w-8 h-8 text-center text-gray-500 hover:bg-gray-100 p-1 rounded-md"
       >
         <BoldIcon className="w-5" />
@@ -109,7 +126,25 @@ const TextEditor = ({ className = '' }: TextEditorProps) => {
     extensions: [
       Color.configure({ types: [TextStyle.name, ListItem.name] }),
       TextStyle,
-      StarterKit
+      StarterKit.configure({
+        bulletList: {
+          keepMarks: true,
+          keepAttributes: false,
+          HTMLAttributes: {
+            class: 'list-disc ml-4'
+          }
+        },
+        orderedList: {
+          keepMarks: true,
+          keepAttributes: false,
+          HTMLAttributes: {
+            class: 'list-decimal ml-4'
+          }
+        },
+      }),
+      Link.configure({
+        openOnClick: true,
+      }),
     ],
     content: `
       <h2>
@@ -150,6 +185,17 @@ const TextEditor = ({ className = '' }: TextEditorProps) => {
         editor={editor}
         className="p-2 border-t-[1px] border-gray-200"
       />
+      <style jsx>{`
+        h1 {
+          font-size: 32px;
+          font-weight: bold;
+        }
+
+        h2 {
+          font-size: 24px;
+          font-weight: bold;
+        }
+      `}</style>
     </div>
   );
 };

a609e2ca744adc49ac835aed8d0be1d5becd2432
myungwoo-Y <auddn6676@naver.com>
2023-08-09 08:08:54 +0900

    feat: add nginx config

--- client/services/base.ts
+++ client/services/base.ts
@@ -1,13 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { addTokenToHeader, getBaseHost } from './common';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 // initialize an empty api service that we'll inject endpoints into later as needed
 export const emptySplitApi = createApi({
   reducerPath: 'baseApi',
   tagTypes: ['Curriculum', 'Lecture', 'Post', 'User', 'Certification', 'Class', 'Application', 'Category'],
   baseQuery: fetchBaseQuery({
     // baseUrl: `${getBaseHost()}`,
-    baseUrl: `http://${typeof window !== "undefined" && window?.location?.hostname}:8080`,
+    baseUrl: isProd ? 'https://api.kfvetp.com' : `http://localhost:8080`,
     prepareHeaders: (headers, { getState }) => {
       return addTokenToHeader(headers, getState);
     },
---
+++ nginx/conf.d
@@ -0,0 +1,30 @@
+server {
+  server_name api.kfvetp.com;
+  
+  root /var;
+
+  location / {
+    proxy_pass http://localhost:8080;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Connection 'upgrade';
+    proxy_set_header Host $host;
+    proxy_cache_bypass $http_upgrade;
+  }
+}
+
+
+server {
+  server_name www.kfvetp.com kfvetp.com
+  
+  root /var;
+
+  location / {
+    proxy_pass http://localhost:3000;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Connection 'upgrade';
+    proxy_set_header Host $host;
+    proxy_cache_bypass $http_upgrade;
+  }
+}

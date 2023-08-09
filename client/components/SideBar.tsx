2e91de4ad83ab45061295928cef640e2c855fc95
myungwoo-Y <auddn6676@naver.com>
2023-08-10 03:11:30 +0900

    feat: sidebar action

--- client/components/SideBar.tsx
+++ client/components/SideBar.tsx
@@ -17,20 +17,23 @@ import { useDispatch } from 'react-redux';
 import { setCategories } from '@/features/post/postSlice';
 import { useGetClassesQuery } from '@/services/class';
 import { setClasses } from '@/features/class/classSlice';
+import { usePathname } from 'next/navigation';
+
+
 
 function SideBar() {
   const { data: categories } = useGetCategoriesQuery();
   const { data: classes } = useGetClassesQuery();
   const [selectedMenu, setSelectedMenu] = useState<'' | 'board' | 'class'>('');
   const dispatch = useDispatch();
-
+  const pathname = usePathname();
   const isSelectBoard = selectedMenu === 'board';
   const isSelectClass = selectedMenu === 'class';
-
+  console.log(pathname);
   useEffect(() => {
     dispatch(setCategories({ categories: categories || [] }));
   }, [categories, dispatch]);
-
+  
   useEffect(() => {
     dispatch(setClasses(classes || []));
   }, [classes, dispatch]);
@@ -48,13 +51,13 @@ function SideBar() {
       </div>
       <Link
         href="/"
-        className="flex text-[16px] text-white items-center mx-3 px-3 py-3 hover:bg-primary rounded-xl"
+        className={`flex text-[16px] text-white items-center mx-3 px-3 py-3 hover:bg-primary rounded-xl ${pathname === '/' ? 'bg-primary' : ''}`}
       >
         <HomeIcon width="18" height="18" className="mr-2" /> HOME
       </Link>
       <Link
         href="/class"
-        className="flex text-[16px] text-white items-center my-2 mx-3 px-3 py-3 hover:bg-primary transition-colors rounded-xl"
+        className={`flex text-[16px] text-white items-center my-2 mx-3 px-3 py-3 hover:bg-primary transition-colors rounded-xl ${pathname === '/class' ? 'bg-primary' : ''}`}
       >
         <DocumentMagnifyingGlassIcon width="18" height="18" className="mr-2" />{' '}
         수강신청
@@ -80,7 +83,7 @@ function SideBar() {
             <Link
               key={data.classId}
               href={`/class/${data.classId}/lectures`}
-              className="flex text-[16px] text-white items-center mx-3 px-3 pl-10 py-3 hover:bg-primary rounded-xl"
+              className={`flex text-[16px] text-white items-center mx-3 px-3 pl-10 py-3 hover:bg-primary rounded-xl ${pathname === '/class/' + data.classId + '/lectures'  ? 'bg-primary' : ''}`}
             >
               {data.title}
             </Link>
@@ -90,13 +93,13 @@ function SideBar() {
         <>
           <Link
             href={`/category/4`}
-            className="flex text-[16px] text-white items-center mx-3 px-3 pl-10 py-3 hover:bg-primary rounded-xl"
+            className={`flex text-[16px] text-white items-center mx-3 px-3 pl-10 py-3 hover:bg-primary rounded-xl ${pathname === '/category/4' ? 'bg-primary' : ''}`}
           >
             자료실
           </Link>
           <Link
             href={`/category/5`}
-            className="flex text-[16px] text-white items-center mx-3 px-3 pl-10 py-3 hover:bg-primary rounded-xl"
+            className={`flex text-[16px] text-white items-center mx-3 px-3 pl-10 py-3 hover:bg-primary rounded-xl ${pathname === '/category/5' ? 'bg-primary' : ''}`}
           >
             교육수료심사
           </Link>
@@ -124,7 +127,7 @@ function SideBar() {
             <Link
               key={category.categoryId}
               href={`/category/${category.categoryId}`}
-              className="flex text-[16px] text-white items-center mx-3 px-3 pl-10 py-3 hover:bg-primary rounded-xl"
+              className={`flex text-[16px] text-white items-center mx-3 px-3 pl-10 py-3 hover:bg-primary rounded-xl ${pathname === '/category/' + category.categoryId ? 'bg-primary' : ''}`}
             >
               {category.name}
             </Link>

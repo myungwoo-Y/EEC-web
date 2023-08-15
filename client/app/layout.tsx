eb7e921c136a1902bddf15b727758a5ea96ce657
myungwoo-Y <auddn6676@naver.com>
2023-08-15 11:26:22 +0900

    feat: add font

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,27 @@ import './globals.css';
 import SideBar from '@/components/SideBar';
 import Nav from '@/components/Nav';
 import { Providers } from '@/redux/provider';
+import localFont from 'next/font/local';
+
+const pretendard = localFont({
+  src: [
+    {
+      path: '../public/font/Pretendard-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/font/Pretendard-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../public/font/Pretendard-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+})
 
 export const metadata = {
   title: '역학교육센터',
@@ -16,7 +37,7 @@ export default function RootLayout({
 
   return (
     <html lang="ko">
-      <body suppressHydrationWarning={true}>
+      <body suppressHydrationWarning={true} className={pretendard.className}>
         <Providers>
           <div className="flex">
             <SideBar />

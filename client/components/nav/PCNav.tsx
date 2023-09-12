5727f63b7a641952535ada318ac1dc334e6fd2bb
myungwoo-Y <auddn6676@naver.com>
2023-09-12 23:47:25 +0900

    feat: validate admin page

--- client/components/nav/PCNav.tsx
+++ client/components/nav/PCNav.tsx
@@ -1,41 +1,46 @@
-'use client'
+'use client';
 
-import { removeCredentials } from '@/features/auth/authSlice'
-import { User } from '@/model/user'
-import { ArrowRightOnRectangleIcon, Cog6ToothIcon, PencilSquareIcon, UserCircleIcon, UserIcon, UserMinusIcon } from '@heroicons/react/24/outline'
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import NavItem from '../NavItem'
-import { getUserRoleName } from '@/lib/user'
+import { removeCredentials } from '@/features/auth/authSlice';
+import { User, UserRole } from '@/model/user';
+import {
+  ArrowRightOnRectangleIcon,
+  Cog6ToothIcon,
+  PencilSquareIcon,
+  UserCircleIcon,
+  UserIcon,
+  UserMinusIcon,
+} from '@heroicons/react/24/outline';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import NavItem from '../NavItem';
+import { getUserRoleName } from '@/lib/user';
 
 type PCNavProps = {
-  user: User | null
-}
+  user: User | null;
+};
 
-function PCNav({ user }: PCNavProps ) {
+function PCNav({ user }: PCNavProps) {
   const dispatch = useDispatch();
 
   return (
     <div className="flex w-full h-14 py-4 flex-row-reverse px-8 gap-4">
       {user ? (
         <>
-          <button 
+          <button
             className="flex items-center hover:text-primary"
             onClick={() => dispatch(removeCredentials())}
           >
             <UserMinusIcon className="w-4 h-4 mr-1" /> 로그아웃
           </button>
-          <NavItem
-            text="정보수정"
-            path="/update"
-            Icon={PencilSquareIcon}
-          />
-          <NavItem
-            text="서비스관리"
-            path="/admin"
-            Icon={Cog6ToothIcon}
-          />
-          <div className="flex items-center"><UserCircleIcon className="w-4 h-4 mr-1" />{`${user.name}(${getUserRoleName(user.role)})님, 환영합니다`}</div>
+          <NavItem text="정보수정" path="/update" Icon={PencilSquareIcon} />
+          {user?.role === UserRole.ADMIN && (
+            <NavItem text="서비스관리" path="/admin" Icon={Cog6ToothIcon} />
+          )}
+
+          <div className="flex items-center">
+            <UserCircleIcon className="w-4 h-4 mr-1" />
+            {`${user.name}(${getUserRoleName(user.role)})님, 환영합니다`}
+          </div>
         </>
       ) : (
         <>
@@ -53,7 +58,7 @@ function PCNav({ user }: PCNavProps ) {
         </>
       )}
     </div>
-  )
+  );
 }
 
-export default PCNav
+export default PCNav;

6b93ccbbf52465392b4043243f735c5873bf3e9e
myungwoo-Y <auddn6676@naver.com>
2023-09-30 20:31:42 +0900

    fix: config map in client

--- client/lib/configMap.ts
+++ client/lib/configMap.ts
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const configMap = {
   serverUrl: isProd ? 'https://api.kfvetp.com' : 'http://kfvetp.local.com:8080',
-  loginUrl: isProd ? 'https://kfvetp.com/login?redirect=true' : 'http://kfvetp.local.com:3000/login?redirect=true',
+  loginUrl: isProd ? 'https://www.kfvetp.com/login?redirect=true' : 'http://kfvetp.local.com:3000/login?redirect=true',
 };
 
 export default configMap;

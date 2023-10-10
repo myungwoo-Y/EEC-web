f0ad6f5f39f56c53750c354b23eca98d8ddb7bea
myungwoo-Y <auddn6676@naver.com>
2023-10-11 02:31:13 +0900

    fix: fix path name

--- client/app/simple-report/page.tsx
+++ client/app/simple-report/page.tsx
@@ -3,12 +3,88 @@
 import Button from '@/components/Button';
 import Date from '@/components/Date';
 import Input from '@/components/Input';
-import { TBody, THead, Table, Td, Th, Tr } from '@/components/Table';
 import { selectCurrentUser } from '@/features/auth/authSlice';
 import { toInputDate } from '@/lib/date';
 import { useUpdateSimpleReportMutation } from '@/services/user';
-import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import classNames from 'classnames';
+import React, { useEffect, useState, PropsWithChildren } from 'react';
+
+type TableProps = {
+  className?: string;
+};
+
+type TableSpan = {
+  colSpan?: number;
+  rowSpan?: number;
+};
+
+export function TableContainer({
+  className,
+  children,
+}: PropsWithChildren<{ className?: string }>) {
+  return (
+    <div className={classNames('overflow-x-auto overflow-y-auto', className)}>{children}</div>
+  );
+}
+
+export function THead({ className, children }: PropsWithChildren<TableProps>) {
+  return (
+    <thead className={classNames('bg-gray-100', className)}>{children}</thead>
+  );
+}
+
+export function TBody({ className, children }: PropsWithChildren<TableProps>) {
+  return <tbody className={className}>{children}</tbody>;
+}
+
+export function Th({
+  className,
+  children,
+  rowSpan,
+  colSpan,
+}: PropsWithChildren<TableProps & TableSpan>) {
+  return (
+    <th
+      colSpan={colSpan}
+      rowSpan={rowSpan}
+      className={classNames(
+        'border-gray-300 border-[1px] border-t-black py-3',
+        className
+      )}
+    >
+      {children}
+    </th>
+  );
+}
+
+export function Td({
+  className,
+  children,
+  rowSpan,
+  colSpan,
+}: PropsWithChildren<TableProps & TableSpan>) {
+  return (
+    <td
+      colSpan={colSpan}
+      rowSpan={rowSpan}
+      className={classNames(
+        'border-gray-300 border-[1px] border-t-black py-3 text-center',
+        className
+      )}
+    >
+      {children}
+    </td>
+  );
+}
+
+export function Tr({ className, children }: PropsWithChildren<TableProps>) {
+  return <tr className={className}>{children}</tr>;
+}
+
+export function Table({ className, children }: PropsWithChildren<TableProps>) {
+  return <table className={classNames('w-full', className)}>{children}</table>;
+}
 
 function SimpleReport() {
   const user = useSelector(selectCurrentUser);
--- client/components/admin/report/UserResultTable.tsx
+++ client/components/admin/report/UserResultTable.tsx
@@ -1,8 +1,84 @@
-import { TBody, THead, Table, TableContainer, Td, Th, Tr } from '@/components/Table';
 import TextArea from '@/components/TextArea';
 import { toSixBirthday } from '@/lib/date';
 import { User } from '@/model/user';
-import React from 'react';
+import classNames from 'classnames';
+import React, { PropsWithChildren } from 'react';
+
+type TableProps = {
+  className?: string;
+};
+
+type TableSpan = {
+  colSpan?: number;
+  rowSpan?: number;
+};
+
+export function TableContainer({
+  className,
+  children,
+}: PropsWithChildren<{ className?: string }>) {
+  return (
+    <div className={classNames('overflow-x-auto overflow-y-auto', className)}>{children}</div>
+  );
+}
+
+export function THead({ className, children }: PropsWithChildren<TableProps>) {
+  return (
+    <thead className={classNames('bg-gray-100', className)}>{children}</thead>
+  );
+}
+
+export function TBody({ className, children }: PropsWithChildren<TableProps>) {
+  return <tbody className={className}>{children}</tbody>;
+}
+
+export function Th({
+  className,
+  children,
+  rowSpan,
+  colSpan,
+}: PropsWithChildren<TableProps & TableSpan>) {
+  return (
+    <th
+      colSpan={colSpan}
+      rowSpan={rowSpan}
+      className={classNames(
+        'border-gray-300 border-[1px] border-t-black py-3',
+        className
+      )}
+    >
+      {children}
+    </th>
+  );
+}
+
+export function Td({
+  className,
+  children,
+  rowSpan,
+  colSpan,
+}: PropsWithChildren<TableProps & TableSpan>) {
+  return (
+    <td
+      colSpan={colSpan}
+      rowSpan={rowSpan}
+      className={classNames(
+        'border-gray-300 border-[1px] border-t-black py-3 text-center',
+        className
+      )}
+    >
+      {children}
+    </td>
+  );
+}
+
+export function Tr({ className, children }: PropsWithChildren<TableProps>) {
+  return <tr className={className}>{children}</tr>;
+}
+
+export function Table({ className, children }: PropsWithChildren<TableProps>) {
+  return <table className={classNames('w-full', className)}>{children}</table>;
+}
 
 type UserResultTableProps = {
   users: User[] | undefined;
